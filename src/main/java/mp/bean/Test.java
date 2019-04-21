75b5161a9810a97018fc53b98ecbcc09dd3f4234
bruce <bruce>
2019-04-21 10:23:26 +0800

    add ehcache2

--- src/main/java/mp/bean/Test.java
+++ src/main/java/mp/bean/Test.java
@@ -2,6 +2,7 @@ package mp.bean;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,106 +27,98 @@ public class Test {
 			sqlSession = factory.openSession();
 			// Dept dept = sqlSession.selectOne("Dept.getDeptById", 40);
 			DeptMapper mapper = sqlSession.getMapper(DeptMapper.class);
-			
-			//一級快取(SqlSession快取)
+
+			// 一級快取(SqlSession快取)
 			Dept dept1 = mapper.getDeptById(40);
-			//sqlSession.clearCache();清一級快取
-			//增刪改操作之後也不會有快取
+			// sqlSession.clearCache();清一級快取
+			// 增刪改操作之後也不會有快取
 			Dept dept2 = mapper.getDeptById(40);
-			//Dept dept2 = mapper.getDeptByIdAndLoc(40, "BOSTON");
+			// Dept dept2 = mapper.getDeptByIdAndLoc(40, "BOSTON");
 			System.out.println(dept1 == dept2);
+
 			
 			sqlSession.close();
-			
+
 			sqlSession2 = factory.openSession();
 			DeptMapper mapper2 = sqlSession2.getMapper(DeptMapper.class);
 			Dept dept3 = mapper2.getDeptById(40);
-			//System.out.println(dept1 == dept3);
+			// System.out.println(dept1 == dept3);
 			System.out.println(dept1 == dept3);
 			System.out.println(dept2 == dept3);
-			
-			
-			/*
-			Dept dept = mapper.getDeptById(40);
-			System.out.println("dept=" + dept);
-			System.out.println("deptNo=" + dept.getDeptNo());
-			System.out.println("dName=" + dept.getdName());
-			System.out.println("loc=" + dept.getLoc());
-			*/
+			 
 			
 			/*
-			Dept dept = mapper.getDeptByIdAndLoc(40, "BOSTON");
-			System.out.println("dept=" + dept);
-			*/
-			
+			 * Dept dept = mapper.getDeptById(40); System.out.println("dept=" + dept);
+			 * System.out.println("deptNo=" + dept.getDeptNo()); System.out.println("dName="
+			 * + dept.getdName()); System.out.println("loc=" + dept.getLoc());
+			 */
+
 			/*
-			List<Integer> list = new ArrayList<>();
-			list.add(40);
-			Dept dept = mapper.getDeptByList(list);
-			System.out.println("dept=" + dept);
-			*/
-			
+			 * Dept dept = mapper.getDeptByIdAndLoc(40, "BOSTON");
+			 * System.out.println("dept=" + dept);
+			 */
+
 			/*
-			Map<String, Object> map = new HashMap<>();
-			map.put("xxx", 40);
-			map.put("loc", "BOSTON");
-			Dept dept = mapper.getDeptByMap(map);
-			System.out.println("dept=" + dept);
-			*/
-			
+			 * List<Integer> list = new ArrayList<>(); list.add(40); Dept dept =
+			 * mapper.getDeptByList(list); System.out.println("dept=" + dept);
+			 */
+
 			/*
-			List<Dept> listByName = mapper.getListByName("%O%");
-			System.out.println(listByName);//找不到不是null，是空的 list
-			listByName.forEach((dept) -> {
-				System.out.println(dept.getdName());
-			});
-			*/
-			
+			 * Map<String, Object> map = new HashMap<>(); map.put("xxx", 40); map.put("loc",
+			 * "BOSTON"); Dept dept = mapper.getDeptByMap(map); System.out.println("dept=" +
+			 * dept);
+			 */
+
 			/*
-			Map<String, Object> mapById = mapper.getMapById(40);
-			System.out.println(mapById);// 找不到為null
-			*/
-			
+			 * List<Dept> listByName = mapper.getListByName("%O%");
+			 * System.out.println(listByName);//找不到不是null，是空的 list listByName.forEach((dept)
+			 * -> { System.out.println(dept.getdName()); });
+			 */
+
 			/*
-			Map<Integer, Dept> mapByName = mapper.getMapByName("%O%");
-			System.out.println(mapByName);// 找不到不是null，是空的 map
-			*/
-			
-			//Dept deptAndEmp = mapper.getDeptAndEmpsById(10);
+			 * Map<String, Object> mapById = mapper.getMapById(40);
+			 * System.out.println(mapById);// 找不到為null
+			 */
+
 			/*
-			Dept deptAndEmp = mapper.getDept2Step(10);
-			System.out.println(deptAndEmp.getDeptNo());
-			System.out.println(deptAndEmp.getdName());
-			System.out.println(deptAndEmp.getLoc());
-			*/
+			 * Map<Integer, Dept> mapByName = mapper.getMapByName("%O%");
+			 * System.out.println(mapByName);// 找不到不是null，是空的 map
+			 */
+
+			// Dept deptAndEmp = mapper.getDeptAndEmpsById(10);
+			/*
+			 * Dept deptAndEmp = mapper.getDept2Step(10);
+			 * System.out.println(deptAndEmp.getDeptNo());
+			 * System.out.println(deptAndEmp.getdName());
+			 * System.out.println(deptAndEmp.getLoc());
+			 */
 //			deptAndEmp.getEmps().stream().forEach(data -> {
 //				System.out.println("============");
 //				System.out.println(data.getEname());
 //				System.out.println(data.getJob());
 //			});
-			
-			
-			
-			//EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
+
+			// EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
 //			List<Emp> emp = mapper.getEmp();
 //			emp.forEach((e)-> {
 //				System.out.println(e.getEmpno());
 //			});
-			
-			
+
 //			Emp empById = mapper.getEmpById(7369);
 //			Emp empById = mapper.getEmp2Step(7369);
 //			System.out.println(empById.getEmpno());
 //			System.out.println(empById.getDeptno());
-			//System.out.println(empById.getDept());
-			//System.out.println(empById.getDept().getdName());
-			
+			// System.out.println(empById.getDept());
+			// System.out.println(empById.getDept().getdName());
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			sqlSession.close();
+			if (sqlSession != null) {
+				sqlSession.close();
+			}
 			try {
 				if (is != null) {
 					is.close();
